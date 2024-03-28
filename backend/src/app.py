eecb3e82430a7fb303a63f39a6ce1ab9dca88be8
lohopupa <lohopupa@gmail.com>
2024-03-28 23:04:02 +0600

    [BACKEND] Add file_query endpoint

---
+++ backend/src/app.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, UploadFile, File
+from model import Model
+import pandas as pd
+import numpy as np
+from io import BytesIO
+
+router = APIRouter(prefix="/api")
+
+model = Model()
+
+@router.post("/file_query")
+async def file_query(file: UploadFile = File(...)):
+    if not file.filename.endswith(".csv"):
+        return {"error": "File should be csv!"}
+    content = await file.read()
+    print(type(file))
+    try:
+        ds = pd.read_csv(BytesIO(content))
+        data = np.expand_dims(ds.values, axis=0)
+        result = model.predict(data)
+        
+        return {"result": "TRUE" if result == 1 else "FALSE"}
+    except Exception as e:
+        print(e)
+        return {"error": "Something went wrong!"}
---
+++ backend/src/model.py
@@ -0,0 +1,13 @@
+
+from tensorflow.keras.models import load_model
+import pandas as pd
+import numpy as np
+import os
+
+class Model:
+    def __init__(self):
+        modelPath= os.path.join('models', 'fold.h5')
+        self.model = load_model(modelPath)
+        
+    def predict(self, data):
+        return (self.model.predict(data)>0.5).astype("int32")[0]
